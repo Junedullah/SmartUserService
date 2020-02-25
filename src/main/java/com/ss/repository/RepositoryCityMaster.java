a02fe4de6314a75c2a889af91c4044fb02caa73f
Junedullah <61226978+Junedullah@users.noreply.github.com>
2020-02-25 14:47:56 +0530

    Update RepositoryCityMaster.java

--- src/main/java/com/ss/repository/RepositoryCityMaster.java
+++ src/main/java/com/ss/repository/RepositoryCityMaster.java
@@ -9,7 +9,7 @@
  */
 package com.ss.repository;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
